c2ba8398542ea5dbee93a30e2395a8c82c801d43
KrittidejDev <Krittidej.Dev@gmail.com>
2024-09-29 15:23:51 +0700

    edite

--- src/components/NewCrouse.js
+++ src/components/NewCrouse.js
@@ -1,7 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import { FaCaretDown } from "react-icons/fa";
+import { MdPlayLesson, MdPerson } from "react-icons/md";
+import { IoMdTrophy } from "react-icons/io";
+
 import CardImg1 from "../img/academy/10.png";
+import CardImg2 from "../img/academy/04.png";
+import ScoreImg from "../img/Score.png";
 
 const Container = styled.div`
   width: 90%;
@@ -38,9 +43,91 @@ const Container = styled.div`
   .CardBox {
     width: 100%;
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-    .card {
-      width: 300px;
+    margin-top: 40px;
+    grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+    gap: 20px;
+
+    .Card {
+      display: flex;
+      flex-direction: column;
+      width: 350px;
+      margin-top: 20px;
+      padding: 10px;
+      border-radius: 20px;
+      box-shadow: 0 2px 5px rgba(0.1, 0.1, 0.1, 0.1);
+
+      img {
+        width: 100%;
+        height: 200px;
+        object-fit: cover;
+        border-radius: 10px;
+      }
+
+      h3 {
+        font-size: 18px;
+        margin-bottom: 10px;
+      }
+
+      .CardInfo {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        gap: 10px;
+        align-items: center;
+        color: gray;
+        font-size: 14px;
+        margin-bottom: 10px;
+
+        p {
+          display: flex;
+          align-items: center;
+          gap: 5px;
+        }
+      }
+
+      .CardActions {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+
+        button {
+          padding: 10px 24px;
+          border: none;
+          border-radius: 10px;
+          background-color: #080808;
+          color: white;
+          cursor: pointer;
+        }
+
+        .CardScore {
+          position: relative;
+          height: 40px;
+          width: 40px;
+          display: flex;
+          align-items: center;
+
+          img {
+            position: absolute;
+            width: 40px;
+            height: 40px;
+            z-index: 1;
+          }
+
+          span {
+            position: absolute;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            z-index: 2;
+          }
+        }
+      }
     }
   }
 `;
@@ -48,6 +135,51 @@ const Container = styled.div`
 const Crouse = [
   {
     id: 1,
+    title: "Learn Figma - UI/UX Design Essential Training",
+    lesson: 6,
+    student: 198,
+    level: "Beginner",
+    ratting: 4,
+    image: CardImg1,
+  },
+  {
+    id: 2,
+    title: "Python for Beginners - Learn Programming from scratch",
+    lesson: 21,
+    student: 99,
+    level: "Advanced",
+    ratting: 2,
+    image: CardImg2,
+  },
+  {
+    id: 3,
+    title: "Learn Figma - UI/UX Design Essential Training",
+    lesson: 6,
+    student: 198,
+    level: "Beginner",
+    ratting: 4,
+    image: CardImg1,
+  },
+  {
+    id: 4,
+    title: "Learn Figma - UI/UX Design Essential Training",
+    lesson: 6,
+    student: 198,
+    level: "Beginner",
+    ratting: 4,
+    image: CardImg1,
+  },
+  {
+    id: 5,
+    title: "Learn Figma - UI/UX Design Essential Training",
+    lesson: 6,
+    student: 198,
+    level: "Beginner",
+    ratting: 4,
+    image: CardImg1,
+  },
+  {
+    id: 6,
     title: "Learn Figma - UI/UX Design Essential Training",
     lesson: 6,
     student: 198,
@@ -78,16 +210,26 @@ const NewCrouse = () => {
         {Crouse.map((crouse) => (
           <div key={crouse.id} className="Card">
             <img src={crouse.image} alt={crouse.title} />
-            <div className="Card-content">
-              <h3>{crouse.title}</h3>
-              <div className="CardInfo">
-                <p></p>
-                <p></p>
-                <p></p>
-              </div>
-              <div className="CardActions">
-                <button></button>
-                <div className="CardScore"></div>
+            <h3>{crouse.title}</h3>
+            <div className="CardInfo">
+              <p>
+                <MdPlayLesson />
+                Lesson : {crouse.lesson}
+              </p>
+              <p>
+                <MdPerson />
+                student : {crouse.student}
+              </p>
+              <p>
+                <IoMdTrophy />
+                {crouse.level}
+              </p>
+            </div>
+            <div className="CardActions">
+              <button>Start course</button>
+              <div className="CardScore">
+                <img src={ScoreImg} alt="Score" />
+                <span>{crouse.ratting}</span>
               </div>
             </div>
           </div>
